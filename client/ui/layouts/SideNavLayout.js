a2289965fdaf48913c9bc444039ddcb9ca2fe4b3
zhaoyao91 <zhaoyao91@163.com>
2017-04-19 23:13:09 +0800

    add project list layout

--- client/ui/layouts/SideNavLayout.js
+++ client/ui/layouts/SideNavLayout.js
@@ -10,7 +10,7 @@ export default SideNavLayout
 
 const Layout = ({left, main}) => (
   <div style={{display: 'flex', height: '100%', width: '100%'}}>
-    <div style={{width: '300px', height: '100%'}}>{left}</div>
+    <div style={{width: '200px', height: '100%'}}>{left}</div>
     <div style={{width: '100%', height: '100%'}}>{main}</div>
   </div>
 )
--- client/ui/pages/ProjectsPage.js
+++ client/ui/pages/ProjectsPage.js
@@ -1,12 +1,61 @@
 import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
+import { flatten } from 'lodash/fp'
 
 import MainLayout from '../layouts/MainLayout'
 import SideNavLayout from '../layouts/SideNavLayout'
 
+const fakeProjects = [
+  {name: '打到帝国主义'},
+  {name: '中华人民共和国万岁'},
+  {name: '打到帝国主义打到帝国主义'},
+  {name: '中华人民共和国万岁中华人民共和国万岁'},
+  {name: '打到帝国主义打到帝国主义打到帝国主义'},
+  {name: '吃葡萄不吐葡萄皮吃葡萄不吐葡萄皮吃葡萄不吐葡萄皮吃葡萄不吐葡萄皮吃葡萄不吐葡萄皮吃葡萄不吐葡萄皮'},
+]
+
 export default () => (
   <MainLayout>
     <SideNavLayout>
-      <h1>Projects Page</h1>
+      <div style={{width: '100%', height: '100%', padding: '1em'}}>
+        <ProjectListLayout>
+          <AddProjectButton/>
+          {
+            fakeProjects.map(project => <ProjectCard key={project.name} project={project}/>)
+          }
+        </ProjectListLayout>
+      </div>
     </SideNavLayout>
   </MainLayout>
+)
+
+const AddProjectButton = () => (
+  <Card style={{width: '100%', height: '100%'}}>
+    <Card.Content style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Icon name="plus" size="huge"/>
+    </Card.Content>
+  </Card>
+)
+
+const ProjectCard = ({project}) => (
+  <Card style={{width: '100%', height: '100%'}}>
+    <Card.Content>
+      <Card.Header>
+        {project.name}
+      </Card.Header>
+    </Card.Content>
+  </Card>
+)
+
+const ProjectListLayout = ({children}) => (
+  <div>
+    {
+      flatten(children).map((item, index) => (
+        <div key={index}
+             style={{width: '200px', height: '150px', float: 'left', marginRight: '1em', marginBottom: '1em'}}>
+          {item}
+        </div>
+      ))
+    }
+  </div>
 )
