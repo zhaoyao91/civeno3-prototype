3a7e0c9b9b610c2122f0088367dbbff75ced4c5f
zhaoyao91 <zhaoyao91@163.com>
2017-04-20 09:40:41 +0800

    update ProjectsPage styles

--- client/ui/pages/ProjectsPage.js
+++ client/ui/pages/ProjectsPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Card, Icon, Button } from 'semantic-ui-react'
+import { Button, Card, Icon } from 'semantic-ui-react'
 import { flatten } from 'lodash/fp'
-import { compose, withState, withHandlers } from 'recompose'
+import { compose, withHandlers, withState } from 'recompose'
 
 import MainLayout from '../layouts/MainLayout'
 import SideNavLayout from '../layouts/SideNavLayout'
 import CreateProjectModal from '../views/CreateProjectModal'
 
 const fakeProjects = [
-  {name: '打到帝国主义'},
+  {name: '打到帝国主义!'},
   {name: '中华人民共和国万岁'},
   {name: '打到帝国主义打到帝国主义'},
   {name: '中华人民共和国万岁中华人民共和国万岁'},
@@ -20,8 +20,10 @@ export default () => (
   <MainLayout>
     <SideNavLayout>
       <div style={{width: '100%', height: '100%', padding: '1em'}}>
-        <ProjectListLayout>
+        <div style={{marginBottom: '1em'}}>
           <CreateProjectButton/>
+        </div>
+        <ProjectListLayout>
           {
             fakeProjects.map(project => <ProjectCard key={project.name} project={project}/>)
           }
@@ -31,14 +33,6 @@ export default () => (
   </MainLayout>
 )
 
-const CreateProjectUIButton = ({onClick}) => (
-  <Card style={{width: '100%', height: '100%'}} onClick={onClick}>
-    <Card.Content style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <Icon name="plus" size="huge" style={{color: 'initial'}}/>
-    </Card.Content>
-  </Card>
-)
-
 const CreateProjectButton = compose(
   withState('modalVisible', 'setModalVisible', false),
   withHandlers({
@@ -46,14 +40,14 @@ const CreateProjectButton = compose(
     closeModal: ({setModalVisible}) => () => setModalVisible(false)
   })
 )(({modalVisible, openModal, closeModal}) => (
-  <CreateProjectModal trigger={<CreateProjectUIButton onClick={openModal}/>} open={modalVisible} onOpen={openModal}
+  <CreateProjectModal trigger={<Button primary>创建项目</Button>} open={modalVisible} onOpen={openModal}
                       onClose={closeModal}/>
 ))
 
 const ProjectCard = ({project}) => (
-  <Card style={{width: '100%', height: '100%'}}>
+  <Card style={{width: '200px'}}>
     <Card.Content>
-      <Card.Header>
+      <Card.Header style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>
         {project.name}
       </Card.Header>
     </Card.Content>
@@ -65,7 +59,7 @@ const ProjectListLayout = ({children}) => (
     {
       flatten(children).map((item, index) => (
         <div key={index}
-             style={{width: '200px', height: '150px', float: 'left', marginRight: '1em', marginBottom: '1em'}}>
+             style={{float: 'left', marginRight: '1em', marginBottom: '1em'}}>
           {item}
         </div>
       ))
